af55402e7578957fc25f2caf1f00d48261d5f307
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-29 11:41:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
   end
 
   def match(array)
-    array.select {|word| word.split("").sort} == @word.split("").sort}
+    array.select {|word| word.split("").sort == @word.split("").sort}
   end
 
 end
